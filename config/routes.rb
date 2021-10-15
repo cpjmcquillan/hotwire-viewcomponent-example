0a9debb4aec88cb3accf7d0677f45791763ef234
Connor McQuillan <cpjmcquillan@gmail.com>
2021-10-15 12:48:41 +0100

    Generate messages controller
    Use rails generate to create messages controller with an index action.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "messages#index"
 
-  # Almost every application defines a route for the root path ("/") at the top of this file.
-  # root "articles#index"
+  get "/messages", to: "messages#index"
 end
