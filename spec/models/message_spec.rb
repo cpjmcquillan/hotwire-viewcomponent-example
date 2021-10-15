464c74d70c7c9b38145c2514050bca6a614972a7
Connor McQuillan <cpjmcquillan@gmail.com>
2021-10-15 13:03:19 +0100

    Ensure message body is not empty
    Add validation to check that a message's body is present.

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,24 @@
 require "rails_helper"
 
 RSpec.describe Message, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    context "when body is not present" do
+      it "is invalid" do
+        body = ""
+        message = Message.new(body: body)
+
+        expect(message).to be_invalid
+        expect(message.errors.of_kind?(:body, :blank)).to eq true
+      end
+    end
+
+    context "when body is present" do
+      it "is invalid" do
+        body = "some text to display"
+        message = Message.new(body: body)
+
+        expect(message).to be_valid
+      end
+    end
+  end
 end
